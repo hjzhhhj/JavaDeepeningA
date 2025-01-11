0d2c8a96474179fdc0baa9fb0390e1d38f4a498a
hjzhhhj <jhj090120@gmail.com>
2025-01-11 23:55:00 +0900

    도서 관리 프로그램

--- Book.java
+++ Book.java
@@ -1,10 +1,9 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
 public class Book extends JFrame{
     public Book(){
         setTitle("도서 관리 프로그램");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(900,1200);
         setVisible(true);
     }
