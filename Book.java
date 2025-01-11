576f8759227e82ea567311f5388116bfe75d8523
hjzhhhj <jhj090120@gmail.com>
2025-01-11 23:39:14 +0900

    도서 관리 프로그램 만들기

--- Book.java
+++ Book.java
@@ -1,6 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Book{
-    public static void main(String args[]){
+public class Book extends JFrame{
+    public Book(){
+        setTitle("도서 관리 프로그램");
+        setSize(900,1200);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        Book book = new Book();
 
     }
 }
