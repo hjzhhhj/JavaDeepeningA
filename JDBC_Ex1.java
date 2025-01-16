1e0fecdba07d2e84ef0d8323da41b7fc3a7ed16c
hjzhhhj <jhj090120@gmail.com>
2025-01-16 23:57:37 +0900

    스윙스돈까스

--- JDBC_Ex1.java
+++ JDBC_Ex1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package InT2;
 
 import java.sql.*;
@@ -19,26 +18,4 @@ public class JDBC_Ex1 {
 			System.out.println("DB 연결 오류");
 		}
 	}
-=======
-package InT2;
-
-import java.sql.*;
-
-public class JDBC_Ex1 {
-	public static void main(String[] args) {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection(
-				    "jdbc:mysql://localhost:3306/sampledb?useSSL=false&serverTimezone=UTC", 
-				    "root", 
-				    "1225"
-				);
-			System.out.println("DB 연결 완료");
-		} catch (ClassNotFoundException e) {
-			System.out.println("JDBC 드라이버 로드 에러");
-		} catch (SQLException e) {
-			System.out.println("DB 연결 오류");
-		}
-	}
->>>>>>> 83d75249f105737b1666f0b2ef81e7e4d1282859
 }
--- JDBC_Ex2.java
+++ JDBC_Ex2.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package InT2;
 
 import java.io.*;
@@ -38,44 +37,3 @@ public class JDBC_Ex2 {
       }
    }
 }
-=======
-package InT2;
-
-import java.io.*;
-import java.sql.*;
-
-public class JDBC_Ex2 {
-   public static void main(String[] args) {
-      Connection conn;
-      Statement stmt = null;
-      try {
-    	 Class.forName("com.mysql.cj.jdbc.Driver");
-         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb", "root", "1225");
-         System.out.println("DB 연결 완료");
-         stmt = conn.createStatement();
-         ResultSet srs = stmt.executeQuery("select * from student");
-         printData(srs, "name", "id", "dept");
-         srs = stmt.executeQuery("select name, id, dept from student where name='0|7|x|'");
-         printData(srs, "name", "id", "dept");
-      } catch (ClassNotFoundException e) {
-         System.out.println("JDBC 드라이버 로드 에러");
-      } catch (SQLException e) {
-         System.out.println("SQL 실행 에러");
-      }
-   }
-
-   private static void printData(ResultSet srs, String col1, String col2, String col3) throws SQLException {
-      while (srs.next()) {
-         if (!col1.equals(""))
-            System.out.print(srs.getString("name"));
-         if (!col2.equals(""))
-            System.out.print("\t|\t" + srs.getString("id"));
-         if (!col3.equals(""))
-            System.out.println("\t|\t" + srs.getString("dept"));
-         else
-            System.out.println();
-
-      }
-   }
-}
->>>>>>> 83d75249f105737b1666f0b2ef81e7e4d1282859
